d5915219bb1740f58cdc933f9f58414a52ea9bd1
Krzychu Sieradzki <krzysztofsieradzki00@gmail.com>
2020-03-20 11:34:30 +0100

    beginning

---
+++ src/main/java/com/pl/kaizen/KaizenApplication.java
@@ -0,0 +1,13 @@
+package com.pl.kaizen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaizenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaizenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pl/kaizen/KaizenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pl.kaizen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaizenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
